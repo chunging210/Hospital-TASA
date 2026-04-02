f4f6c0d7dde1f8506f077785581ab2275ccbdcb1
Davin <chunging210@gmail.com>
2026-04-02 09:45:24 +0800

    刪除查詢版本A

--- Controllers/Mvc/PublicController.cs
+++ Controllers/Mvc/PublicController.cs
@@ -7,18 +7,9 @@ namespace TASA.Controllers.Mvc
     public class PublicController : Controller
     {
         /// <summary>
-        /// 會議室空檔查詢頁面（公開）- 版本 A（卡片式）
+        /// 會議室空檔查詢頁面（公開）
         /// </summary>
         [Route("availability")]
-        public IActionResult Availability()
-        {
-            return View();
-        }
-
-        /// <summary>
-        /// 會議室空檔查詢頁面（公開）- 版本 B（時間軸）
-        /// </summary>
-        [Route("availability2")]
         public IActionResult Availability2()
         {
             return View();
